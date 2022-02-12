cf230b16937e7ace461ec5aec9b91ebeb9c11f3e
resulbdev <resulbozdemirr@gmail.com>
2022-02-12 23:12:36 +0300

    Update detail_page.dart

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,3 +1,4 @@
+import 'package:better_future/Custom_dialog.dart';
 import 'package:better_future/Detail_cards.dart';
 import 'package:better_future/constans/text_style.dart';
 import 'package:better_future/video_items.dart';
@@ -72,7 +73,7 @@ class DetailPage extends StatelessWidget {
                                     child: Text("BAĞIŞ YAP",
                                         style: TextStyle(
                                             color: Colors.black,
-                                            fontSize: 20))),
+                                            fontSize: 21))),
                               ),
                             ],
                           ),
@@ -84,85 +85,6 @@ class DetailPage extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-}
-
-class CustomDialog extends StatelessWidget {
-  final String title, description;
-
-  CustomDialog({
-    required this.title,
-    required this.description,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Dialog(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
-      elevation: 0,
-      backgroundColor: Colors.transparent,
-      child: dialogContent(context),
-    );
-  }
-
-  dialogContent(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          padding: EdgeInsets.only(
-            top: 100,
-            bottom: 16,
-            left: 16,
-            right: 16,
-          ),
-          margin: EdgeInsets.only(top: 16),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              shape: BoxShape.rectangle,
-              borderRadius: BorderRadius.circular(17),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black12,
-                  blurRadius: 10.0,
-                  offset: Offset(0.0, 10.0),
-                ),
-              ]),
-          child: Column(mainAxisSize: MainAxisSize.min, children: [
-            Text(
-              title,
-              style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.w200),
-            ),
-            SizedBox(height: 16.0),
-            Text(
-              description,
-              style: TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
-            SizedBox(height: 24.0),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: TextButton(
-                child: Text("Okey"),
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-              ),
-            ),
-          ]),
-        ),
-        Positioned(
-          top: 0,
-          left: 16,
-          right: 16,
-          child: CircleAvatar(
-            backgroundColor: Colors.blueGrey,
-            radius: 50,
-            backgroundImage: AssetImage("assets/tik.png"),
-          ),
-        )
-      ],
     );
   }
 }
