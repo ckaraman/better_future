607de3ae9769d65c8aeb08d91cd8acfa8042a3b3
ckaraman <cagataykaraman89@hotmail.com>
2022-10-23 16:51:11 +0300

    main dart minor changes

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,12 @@ Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
